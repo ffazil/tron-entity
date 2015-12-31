87a984b192818ff133ecd395f3ba285e291334b5
sadath <ssmdrepo@gmail.com>
2015-12-31 10:21:32 +0530

    changed to v0.4 Added JsonIgnore Annotation.

--- src/main/java/com/tracebucket/tron/domain/BaseEntity.java
+++ src/main/java/com/tracebucket/tron/domain/BaseEntity.java
@@ -1,14 +1,20 @@
 package com.tracebucket.tron.domain;
 
-import javax.persistence.*;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.persistence.*;
 
 /**
  * @author ffl
  * @since 13-01-2015
  * @version 0.1
  */
-
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable{
 
@@ -17,9 +23,11 @@ public abstract class BaseEntity implements Serializable{
             @AttributeOverride(name = "entityId", column = @Column(name = "ID", nullable = false))})
     protected EntityId entityId;
 
+    @JsonIgnore
     @Version
     private Long version;
 
+    @JsonIgnore
     @Column(name = "PASSIVE", nullable = false, columnDefinition = "boolean default false")
     @Basic(fetch = FetchType.EAGER)
     private boolean passive;
